446ff6e04484d7e5d0f94514ae464fbecd4c041c
Steven Kryskalla <skryskalla@gmail.com>
2020-12-17 00:21:42 -0500

    make city input onChange trigger an AJAX call to OpenWeather
    And add some placeholders for the other components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+const OPEN_WEATHER_MAP_API_KEY = "fae58d081df939c92e507d70ab7e8a90";
+const OPEN_WEATHER_MAP_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div id="App">
+            <CityInput />
+            <CurrentConditions />
+            <TemperatureGraph />
+            <Forecast />
+        </div>
+    );
+}
+class CityInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            apiResponse: undefined,
+        };
+    }
+
+    cityChanged = (e) => {
+        fetch(OPEN_WEATHER_MAP_WEATHER_URL + "q=" + encodeURIComponent(e.target.value) + "&appid=" + OPEN_WEATHER_MAP_API_KEY)
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({apiResponse: data});
+            });
+    }
+
+    render() {
+        return (
+            <div id="CityInput">
+                <label htmlFor="city">Your city</label>
+                <input name="city" id="city" onChange={this.cityChanged} />
+            </div>
+        );
+    }
+}
+
+
+class CurrentConditions extends React.Component {
+    render() {
+        return (
+            <div id="CurrentConditions">
+                <p>11:27PM, Wed, Dec 16, 2020</p>
+                <p>Cloudy, 25F</p>
+                <p>84% humidity, 8mph wind speed</p>
+            </div>
+        );
+    }
+}
+
+class TemperatureGraph extends React.Component {
+    render() {
+        return (
+            <div id="TemperatureGraph">
+                25F
+            </div>
+        );
+    }
+}
+
+class Forecast extends React.Component {
+    render() {
+        return (
+            <div id="Forecast">
+                <p>Today, cloudy, 84% humidity</p>
+                <p>Dec 17, cloudy, 85% humidity</p>
+                <p>Dec 18, cloudy, 86% humidity</p>
+                <p>Dec 19, cloudy, 87% humidity</p>
+            </div>
+        );
+    }
 }
 
 export default App;
