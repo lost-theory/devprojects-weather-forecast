1a21e11b7e194e4e193f7dfde0b3515c2a7c4c62
Steven Kryskalla <skryskalla@gmail.com>
2020-12-18 00:01:47 -0500

    current weather & forecast AJAX calls working + parent/child communication

--- src/App.js
+++ src/App.js
@@ -2,31 +2,98 @@ import React from 'react';
 import './App.css';
 
 const OPEN_WEATHER_MAP_API_KEY = "fae58d081df939c92e507d70ab7e8a90";
-const OPEN_WEATHER_MAP_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?";
+const OPEN_WEATHER_MAP_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=" + OPEN_WEATHER_MAP_API_KEY + "&";
+const OPEN_WEATHER_MAP_FORECAST_URL = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&appid=" + OPEN_WEATHER_MAP_API_KEY + "&";
+const FORECAST_DAYS = 4;
 
 function App() {
-    return (
-        <div id="App">
-            <CityInput />
-            <CurrentConditions />
-            <TemperatureGraph />
-            <Forecast />
-        </div>
-    );
+    return <WeatherForecastApp />;
 }
-class CityInput extends React.Component {
+
+class WeatherForecastApp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            apiResponse: undefined,
+            weatherData: {
+                temp: undefined,
+                condition: undefined,
+                windSpeed: undefined,
+                humidity: undefined,
+            },
+            forecastData: {
+                daily: undefined,
+            },
         };
     }
 
+    currentTime() {
+        var d = new Date();
+        var time = d.toLocaleTimeString();
+        time = time.replace(/(\d+):(\d+):(\d+)/, "$1:$2");
+        var day = d.toDateString();
+        return time + ", " + day;
+    }
+
+    handleWeatherApiResponse = (res) => {
+        var weatherData = {};
+        if(res.main) {
+            weatherData = {
+                temp: parseInt(res.main.temp),
+                condition: res.weather[0].main,
+                windSpeed: parseInt(res.wind.speed),
+                humidity: res.main.humidity,
+            };
+        }
+        this.setState({weatherData: weatherData});
+    }
+
+    handleForecastApiResponse = (res) => {
+        var forecastData = {};
+        if(res.daily) {
+            forecastData = {
+                daily: res.daily,
+            };
+        }
+        this.setState({forecastData: forecastData});
+    }
+
+    render() {
+        return (
+            <div id="App">
+                <CityInput
+                    handleWeatherApiResponse={this.handleWeatherApiResponse}
+                    handleForecastApiResponse={this.handleForecastApiResponse}
+                />
+                <CurrentConditions
+                    currentTime={this.currentTime()}
+                    weatherData={this.state.weatherData}
+                />
+                <TemperatureGraph
+                    weatherData={this.state.weatherData}
+                    forecastData={this.state.forecastData}
+                />
+                <Forecast
+                    forecastData={this.state.forecastData}
+                />
+            </div>
+        );
+    }
+}
+
+class CityInput extends React.Component {
     cityChanged = (e) => {
-        fetch(OPEN_WEATHER_MAP_WEATHER_URL + "q=" + encodeURIComponent(e.target.value) + "&appid=" + OPEN_WEATHER_MAP_API_KEY)
+        fetch(OPEN_WEATHER_MAP_WEATHER_URL + "q=" + encodeURIComponent(e.target.value))
             .then(res => res.json())
             .then((data) => {
-                this.setState({apiResponse: data});
+                this.props.handleWeatherApiResponse(data);
+                if(!data.coord) {
+                    return;
+                }
+                fetch(OPEN_WEATHER_MAP_FORECAST_URL + "lat=" + encodeURIComponent(data.coord.lat) + "&lon=" + encodeURIComponent(data.coord.lon))
+                    .then(res => res.json())
+                    .then((data) => {
+                        this.props.handleForecastApiResponse(data);
+                    });
             });
     }
 
@@ -40,14 +107,13 @@ class CityInput extends React.Component {
     }
 }
 
-
 class CurrentConditions extends React.Component {
     render() {
         return (
             <div id="CurrentConditions">
-                <p>11:27PM, Wed, Dec 16, 2020</p>
-                <p>Cloudy, 25F</p>
-                <p>84% humidity, 8mph wind speed</p>
+                <p>{this.props.currentTime}</p>
+                <p>{this.props.weatherData.condition || '--'}, {this.props.weatherData.temp || '--'}&deg;F</p>
+                <p>{this.props.weatherData.humidity || '--'}% humidity, {this.props.weatherData.windSpeed || '--'}mph wind speed</p>
             </div>
         );
     }
@@ -57,7 +123,7 @@ class TemperatureGraph extends React.Component {
     render() {
         return (
             <div id="TemperatureGraph">
-                25F
+                {this.props.weatherData.temp || '--'}&deg;F
             </div>
         );
     }
@@ -65,12 +131,26 @@ class TemperatureGraph extends React.Component {
 
 class Forecast extends React.Component {
     render() {
+        if(!this.props.forecastData.daily) {
+            return <div id="Forecast"><p>--</p></div>;
+        }
+        var days = [];
+        var day, temp, humid, condition;
+        for(var i=0; i < FORECAST_DAYS; i++) {
+            if(i === 0) {
+                day = "Today";
+            } else {
+                day = new Date(this.props.forecastData.daily[i].dt * 1000).toDateString().split(' ');
+                day = day[1] + " " + day[2];
+            }
+            temp = parseInt(this.props.forecastData.daily[i].temp.day);
+            humid = this.props.forecastData.daily[i].humidity;
+            condition = this.props.forecastData.daily[i].weather[0].main;
+            days.push(<p key={day}>{day}, {condition}, {temp}&deg;F, Humidity {humid}%</p>);
+        }
         return (
             <div id="Forecast">
-                <p>Today, cloudy, 84% humidity</p>
-                <p>Dec 17, cloudy, 85% humidity</p>
-                <p>Dec 18, cloudy, 86% humidity</p>
-                <p>Dec 19, cloudy, 87% humidity</p>
+                {days}
             </div>
         );
     }
